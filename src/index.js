476dc840fcc5436921702fdb761ec650c02bb765
Alexey Azarko <Azarkoav@gmail.com>
2021-03-13 23:26:42 +0300

    init: start human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function toReadable (number) {
-  
+    let numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let dozens = ['zero', 'ten','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let a = number % 10;
+    let b = number % 100;
+
+    if (number < 20) return numbers[number];
+
+    if (number < 100) {
+        if (a == 0) return dozens[number / 10];
+        return dozens[(number - a) / 10] + ' ' + numbers[a];
+    }
+
+    if (number < 1000) {
+        if (b == 0) return numbers[number / 100] + ' ' + 'hundred';
+        if (a == 0) return numbers[(number - b) / 100] + ' ' + 'hundred' + ' ' + dozens[b / 10];
+        if (b < 20) return numbers[(number - b) / 100] + ' ' + 'hundred' + ' ' + numbers[b];
+        return numbers[(number - b) / 100] + ' ' + 'hundred' + ' ' + dozens[((number - a) % 100) / 10] + ' ' + numbers[a];
+    }
 }
